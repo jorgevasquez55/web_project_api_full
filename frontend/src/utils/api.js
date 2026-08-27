c026b0a7bd78693361d14dd8e98ca9f9221239bc
jorgevasquez55 <160081461+jorgevasquez55@users.noreply.github.com>
2026-08-26 20:22:13 -0400

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import Api from "../components/Api.js";
 const token = localStorage.getItem("token");
 const api = new Api({
-  address: "https://api.aroundeua.aroundproject.cloudns.be/",
+  address: "https://around-jorge.abrdns.com/",
   token: token,
 });
 export default api;
